aa8f56dfc55ddfbda3561eb8bc10670cbdaf3709
Jack Gong <jackgong48@gmail.com>
2024-05-24 22:14:45 +1200

    ADDED: robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astromedia.dev",
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   vite: {
     ssr: {
       external: ["svgo"],
