f81e56e5358dc633d1eb0a00dc6e4e2b0310b0f3
Jack Gong <jackgong48@gmail.com>
2024-05-29 15:18:19 +1200

    CHANGED: baseurl

--- src/components/CostsCalculator.tsx
+++ src/components/CostsCalculator.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const CostsCalculator = () => {
   // config:
@@ -17,26 +17,74 @@ const CostsCalculator = () => {
   //   - number of hours on your end
   //   - how much money you would save by hiring a professional
 
+  const [numOfPages, setNumOfPages] = useState(3);
+  const [hourlyRate, setHourlyRate] = useState(30);
+  const [wantsPhotography, setWantsPhotography] = useState(false);
+  const [wantsBusinessListing, setWantsBusinessListing] = useState(false);
+  const [websiteType, setWebsiteType] = useState("Personal Portfolio");
+
   const submit = (e: any) => {
     e.preventDefault();
-    alert("hi");
+
+    let bigAgencyCost = 1799;
+    let smallAgencyCost = 500;
+
+    let personalHours = 0;
+    let personalCost = 0;
+
+    if (numOfPages > 5) {
+      bigAgencyCost += (numOfPages - 5) * 150;
+    }
+
+    personalHours = numOfPages * 2;
+
+    if (wantsPhotography) {
+      bigAgencyCost += 100;
+      personalHours += 3;
+    }
+
+    if (wantsBusinessListing) {
+      bigAgencyCost += 100;
+      personalHours += 1;
+    }
+
+    if (websiteType === "Personal Portfolio") {
+      personalHours *= 2;
+    } else if (websiteType === "Service Business") {
+      personalHours *= 3;
+    } else if (websiteType === "E-Commerce") {
+      personalHours *= 4;
+    }
+
+    personalCost = personalHours * hourlyRate;
   };
 
   const userOptions = [
     {
       name: "Number of pages",
       type: "number",
-      value: 3,
+      max: 20,
+      value: numOfPages,
+      handleSetter: setNumOfPages,
     },
     {
       name: "Personal hourly rate",
       type: "number",
-      value: 30,
+      max: 100,
+      value: hourlyRate,
+      handleSetter: setHourlyRate,
     },
     {
       name: "Photography",
       type: "checkbox",
-      value: false,
+      value: wantsPhotography,
+      handleSetter: setWantsPhotography,
+    },
+    {
+      name: "Google Business Listing",
+      type: "checkbox",
+      value: wantsBusinessListing,
+      handleSetter: setWantsBusinessListing,
     },
     {
       name: "website difficulty",
@@ -52,8 +100,13 @@ const CostsCalculator = () => {
           name: "E-Commerce",
         },
       ],
+      handleSetter: setWebsiteType,
     },
   ];
+
+  useEffect(() => {
+    console.log(numOfPages, hourlyRate, wantsPhotography, websiteType);
+  }, [numOfPages, hourlyRate, wantsPhotography, websiteType]);
 
   return (
     <div>
@@ -65,30 +118,50 @@ const CostsCalculator = () => {
           return (
             <div
               key={index}
-              className="grid w-full grid-cols-2 place-items-start"
+              className="grid w-full grid-cols-2 place-items-start gap-4"
             >
               <label className="font-medium capitalize text-2xl">
                 {option.name}
               </label>
               {option.type === "number" ? (
-                <input
-                  type="number"
-                  defaultValue={option.value as number}
-                  className="h-full border-2 border-neutral p-4 text-left"
-                  min={1}
-                />
+                <div className="flex w-full flex-col items-center gap-2">
+                  <input
+                    type="range"
+                    min={0}
+                    max={option.max}
+                    defaultValue={option.value}
+                    onChange={(e) =>
+                      option.handleSetter(parseInt(e.target.value))
+                    }
+                    className="range"
+                  />
+                  <div className="flex flex-row gap-0 whitespace-pre text-xl">
+                    <p>{option.name == "Personal hourly rate" && "$"}</p>
+                    <p>{option.value}</p>
+                    <p>{option.name != "Personal hourly rate" && " pages"}</p>
+                  </div>
+                </div>
               ) : option.type === "checkbox" ? (
-                <input type="checkbox" className="aspect-square h-full " />
+                <input
+                  type="checkbox"
+                  className="aspect-square h-full "
+                  onChange={(e) => option.handleSetter(e.target.checked)}
+                />
               ) : option.type === "select" ? (
                 <div className="flex h-full flex-row gap-8">
                   <select
                     name={option.name}
                     key={index}
-                    className="flex gap-2 border-2 border-neutral"
+                    className="select border-2 border-neutral"
+                    onChange={(e) => option.handleSetter(e.target.value)}
                   >
                     {option.values &&
                       option.values.map((value, index) => {
-                        return <option value={value.name}>{value.name}</option>;
+                        return (
+                          <option value={value.name} key={value.name}>
+                            {value.name}
+                          </option>
+                        );
                       })}
                   </select>
                 </div>
@@ -96,9 +169,6 @@ const CostsCalculator = () => {
             </div>
           );
         })}
-        <button type="submit" className="btn-primary btn mt-8 w-fit">
-          Calculate
-        </button>
       </form>
     </div>
   );
