11b395b0091b97c42629f8f2444dfa3844e45741
Jack Gong <jackgong48@gmail.com>
2024-05-29 20:46:31 +1200

    CHANGED: col to row for calc results

--- src/components/CostsCalculator.tsx
+++ src/components/CostsCalculator.tsx
@@ -17,47 +17,72 @@ const CostsCalculator = () => {
   //   - number of hours on your end
   //   - how much money you would save by hiring a professional
 
-  const [numOfPages, setNumOfPages] = useState(3);
-  const [hourlyRate, setHourlyRate] = useState(30);
-  const [wantsPhotography, setWantsPhotography] = useState(false);
-  const [wantsBusinessListing, setWantsBusinessListing] = useState(false);
-  const [websiteType, setWebsiteType] = useState("Personal Portfolio");
+  const [numOfPages, setNumOfPages] = useState<number>(5);
+  const [hourlyRate, setHourlyRate] = useState<number>(30);
+  const [wantsPhotography, setWantsPhotography] = useState<boolean>(true);
+  const [wantsBusinessListing, setWantsBusinessListing] =
+    useState<boolean>(false);
+  const [websiteType, setWebsiteType] = useState<string>("Personal Portfolio");
+  const [personalCost, setPersonalCost] = useState(0);
+  const [bigAgencyCost, setBigAgencyCost] = useState(1799);
+  const [smallAgencyCost, setSmallAgencyCost] = useState(500);
 
-  const submit = (e: any) => {
-    e.preventDefault();
-
-    let bigAgencyCost = 1799;
-    let smallAgencyCost = 500;
-
-    let personalHours = 0;
-    let personalCost = 0;
+  // Calculate costs and hours
+  const calculateCosts = () => {
+    let baseBigAgencyCost = 1799;
+    let baseSmallAgencyCost = 500;
+    let hours = 0;
 
     if (numOfPages > 5) {
-      bigAgencyCost += (numOfPages - 5) * 150;
+      baseBigAgencyCost += (numOfPages - 5) * 150;
     }
 
-    personalHours = numOfPages * 2;
-
     if (wantsPhotography) {
-      bigAgencyCost += 100;
-      personalHours += 3;
+      baseBigAgencyCost += 100;
+      hours += 3;
     }
 
     if (wantsBusinessListing) {
-      bigAgencyCost += 100;
-      personalHours += 1;
+      baseBigAgencyCost += 100;
+      hours += 1;
     }
 
-    if (websiteType === "Personal Portfolio") {
-      personalHours *= 2;
-    } else if (websiteType === "Service Business") {
-      personalHours *= 3;
-    } else if (websiteType === "E-Commerce") {
-      personalHours *= 4;
+    switch (websiteType) {
+      case "Personal Portfolio":
+        hours += numOfPages * 1 + 20;
+        break;
+      case "Service Business":
+        hours += numOfPages * 1.5 + 40;
+        break;
+      case "E-Commerce":
+        hours += numOfPages * 2 + 100;
+        break;
+      default:
+        break;
     }
 
-    personalCost = personalHours * hourlyRate;
+    // maintainance cost in the first year
+    baseBigAgencyCost += 1400; //https://nzdigital.co.nz/web-design-price-guide-new-zealand/
+    baseSmallAgencyCost += 12 * 50;
+    const userPersonalCost = hours * hourlyRate + 36 * 12;
+
+    setBigAgencyCost(baseBigAgencyCost);
+    setSmallAgencyCost(baseSmallAgencyCost);
+    setPersonalCost(userPersonalCost);
   };
+
+  useEffect(() => {
+    calculateCosts();
+  }, [
+    numOfPages,
+    hourlyRate,
+    wantsPhotography,
+    wantsBusinessListing,
+    websiteType,
+  ]);
+
+  const smallAgencyCostProfit = personalCost - smallAgencyCost;
+  const bigAgencyCostProfit = personalCost - bigAgencyCost;
 
   const userOptions = [
     {
@@ -104,23 +129,20 @@ const CostsCalculator = () => {
     },
   ];
 
-  useEffect(() => {
-    console.log(numOfPages, hourlyRate, wantsPhotography, websiteType);
-  }, [numOfPages, hourlyRate, wantsPhotography, websiteType]);
+  // useEffect(() => {
+  //   console.log(numOfPages, hourlyRate, wantsPhotography, websiteType);
+  // }, [numOfPages, hourlyRate, wantsPhotography, websiteType]);
 
   return (
-    <div>
-      <form
-        onSubmit={(e) => submit(e)}
-        className="mx-auto flex w-full max-w-screen-md flex-col items-center gap-4"
-      >
+    <div className="mx-auto flex w-full flex-row items-center gap-16">
+      <form className="mx-auto flex w-full flex-col items-center gap-4">
         {userOptions.map((option, index) => {
           return (
             <div
               key={index}
-              className="grid w-full grid-cols-2 place-items-start gap-4"
+              className="grid w-full grid-cols-2 items-start gap-4"
             >
-              <label className="font-medium capitalize text-2xl">
+              <label className="font-medium capitalize text-2xl max-md:text-lg">
                 {option.name}
               </label>
               {option.type === "number" ? (
@@ -129,13 +151,11 @@ const CostsCalculator = () => {
                     type="range"
                     min={0}
                     max={option.max}
-                    defaultValue={option.value}
-                    onChange={(e) =>
-                      option.handleSetter(parseInt(e.target.value))
-                    }
+                    defaultValue={String(option.value)}
+                    onChange={(e) => option.handleSetter(e.target.value)}
                     className="range"
                   />
-                  <div className="flex flex-row gap-0 whitespace-pre text-xl">
+                  <div className="flex flex-row gap-0 whitespace-pre text-xl max-md:text-sm">
                     <p>{option.name == "Personal hourly rate" && "$"}</p>
                     <p>{option.value}</p>
                     <p>{option.name != "Personal hourly rate" && " pages"}</p>
@@ -144,7 +164,8 @@ const CostsCalculator = () => {
               ) : option.type === "checkbox" ? (
                 <input
                   type="checkbox"
-                  className="aspect-square h-full "
+                  className="checkbox my-auto aspect-square"
+                  defaultChecked={option.value}
                   onChange={(e) => option.handleSetter(e.target.checked)}
                 />
               ) : option.type === "select" ? (
@@ -170,6 +191,58 @@ const CostsCalculator = () => {
           );
         })}
       </form>
+      <div className="flex flex-col items-center gap-8 text-center">
+        <h1 className="text-2xl max-md:text-lg">
+          By letting a website agency make your website...
+        </h1>
+        <div className="text-xl">
+          {bigAgencyCostProfit > 0 ? (
+            <p className="flex w-full flex-col items-center gap-4 max-md:gap-2">
+              You would save
+              <span className="font-bold text-green-500 text-6xl">
+                ${Math.round(bigAgencyCostProfit)}
+              </span>
+              <span className="italic text-base max-md:text-xs">
+                *with a big agency
+              </span>
+            </p>
+          ) : (
+            <p className="flex w-full flex-col items-center gap-4 max-md:gap-2">
+              You would lose
+              <span className="font-bold text-red-500 text-6xl">
+                -${Math.round(bigAgencyCostProfit) * -1}
+              </span>
+              <span className="italic text-base max-md:text-xs">
+                *with a big agency
+              </span>
+            </p>
+          )}
+          {smallAgencyCostProfit > 0 ? (
+            <p className="flex w-full flex-col items-center gap-4 max-md:gap-2">
+              And you would save
+              <span className="font-bold text-green-500 text-6xl">
+                ${Math.round(smallAgencyCostProfit)}
+              </span>
+              <span className="italic text-base  max-md:text-xs">
+                *with a small agency
+              </span>
+            </p>
+          ) : (
+            <p className="flex w-full flex-col items-center gap-4 max-md:gap-2">
+              And you would lose
+              <span className="font-bold text-red-500 text-6xl">
+                -${Math.round(smallAgencyCostProfit) * -1}
+              </span>
+              <span className="italic text-base  max-md:text-xs">
+                *with a small agency
+              </span>
+            </p>
+          )}
+        </div>
+        <h1 className="mx-auto w-fit  bg-primary p-4 text-center font-extrabold italic text-2xl max-md:p-2 max-md:text-lg">
+          In the first year
+        </h1>
+      </div>
     </div>
   );
 };
